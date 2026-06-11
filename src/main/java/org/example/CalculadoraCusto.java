d3c30806653c8c108088b079cfa4a0dd30397ac2
larissasantos-dev <larissa.santosdev7@gmail.com>
2026-06-11 20:48:31 -0300

    feat: versao para entrega

--- src/main/java/org/example/CalculadoraCusto.java
+++ src/main/java/org/example/CalculadoraCusto.java
@@ -7,31 +7,31 @@ public class CalculadoraCusto {
 
     public double custoMaquinaHora(Impressora3D objImpressora, double horas_uso){
         double horas_totais = 2 * 365 * horas_uso;
-        return (objImpressora.getPrecoImpressora() / horas_totais); // retorna o que seria a variavel custo_maquina_por_hora
+        return (objImpressora.getPrecoImpressora() / horas_totais);
     }
 
     public double custoMaterial(MaterialImpressao objMaterialImpressora, double gramas_utilizadas){
-        return gramas_utilizadas * objMaterialImpressora.getCustoPorGrama(); // retorna o que seria a variavel custo_material
+        return gramas_utilizadas * objMaterialImpressora.getCustoPorGrama();
     }
 
     public double custoEnergia(Impressora3D objImpressora, ProjetoImpressao objProjeto, double valor_kwh){
-        return ((objImpressora.getPotencia_WattsImpressora() / 1000.0) * objProjeto.getHorasImpressao()) * valor_kwh; // retorna o que seria a variavel custo_energia
+        return ((objImpressora.getPotencia_WattsImpressora() / 1000.0) * objProjeto.getHorasImpressao()) * valor_kwh;
     }
 
     public double custoMaoDeObra(ProjetoImpressao objProjeto) {
-        return objProjeto.getHorasImpressao() * objProjeto.getValorMaoDeObra(); // retorna o que seria a variavel custo_mao_obra
+        return objProjeto.getHorasImpressao() * objProjeto.getValorMaoDeObra();
     }
 
     public double taxaFalha(double material_previsto){
-        return material_previsto * 1.10; // retorna o que seria a variavel material_real
+        return material_previsto * 1.10;
     }
 
     public double margemLucro(double custo_total, double margemPercent) {
-        return custo_total * (1 + margemPercent / 100); // retorna o que seria a variavel valor_venda
+        return custo_total * (1 + margemPercent / 100);
     }
 
     public double custoTotal(double custo_material, double custo_maquina, double custo_energia, double custo_mao_manutencao, double custo_mao_obra){
-        return custo_material + custo_maquina + custo_energia + custo_mao_manutencao + custo_mao_obra; // retorna o que seria a variavel custo_total_final
+        return custo_material + custo_maquina + custo_energia + custo_mao_manutencao + custo_mao_obra;
     }
 
     public double custoManutencao(double horasImpressao) {
